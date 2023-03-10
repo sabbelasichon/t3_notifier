d65fc60ded03df41a00c831ede6b933083ed8a12
Sebastian Schreiber <me@schreibersebastian.de>
2023-03-10 12:25:00 +0100

    Initial commit

---
+++ Configuration/Services.php
@@ -0,0 +1,17 @@
+<?php
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+
+return static function (ContainerConfigurator $containerConfigurator, ContainerBuilder $containerBuilder): void {
+    $services = $containerConfigurator->services();
+    $services->defaults()
+        ->public()
+        ->autowire()
+        ->autoconfigure();
+
+    $services->load('Ssch\\T3Notifier\\', __DIR__ . '/../Classes/');
+
+
+};
+
---
+++ README.md
@@ -0,0 +1,3 @@
+# TYPO3 Symfony notifier adapter
+Integrates Symfony Notifier into TYPO3
+[https://symfony.com/doc/current/notifier.html](https://symfony.com/doc/current/notifier.html)
